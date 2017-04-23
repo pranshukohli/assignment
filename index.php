7aa5b47bb252bd782910bd04260d30db8227bf58
pranshukohli <pranshukohli28@gmail.com>
2017-04-23 14:36:18 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,22 @@
-
 <html>
 <head>
-<title>
-	wastfvhiok;
-</title>
-	</head>	
+<script>
+function addData(str) {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("rval").innerHTML = this.responseText;
+            }
+        };
+        xmlhttp.open("GET", "http://motorcon.azurewebsites.net/putdata.php?value=" + str, true);
+        xmlhttp.send();
+    }
+}
+</script>
+</head>
 <body>
-	<p>ajsvdkagsdlu</p>
+
+	<button onclick="addData('11001100')">UP</button>
+<p>Last Added: <span id="rval"></span></p>
 </body>
-	</html>
+</html>
