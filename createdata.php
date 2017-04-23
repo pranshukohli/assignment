1afe2ff7aabada58d0c9d49118e6a3ca1bb204cf
pranshukohli <pranshukohli28@gmail.com>
2017-04-23 15:09:23 +0530

    Update createdata.php

--- createdata.php
+++ createdata.php
@@ -17,7 +17,7 @@ if($conn){
 echo 'bh';
 }
 	$row="q";	
-	$sql = "SELECT value FROM motordata ";
+	$sql = "SELECT value FROM motordata ORDER BY ID DESC LIMIT 2";
 
 	$stmt = sqlsrv_query( $conn, $sql);
 		echo 'jji';
@@ -26,7 +26,7 @@ echo 'bh';
     // output data of each row
 			echo '666';
     while($row = sqlsrv_fetch_array($stmt)) {
-        echo "<br><p>".$row['value']."</p><br>";
+        echo "<p>".$row['value']."</p>";
 	   
     }
 		
