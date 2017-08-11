b85e2c1a98f9024b29b1e69310851924fedfd1ae
pranshukohli <pranshukohli28@gmail.com>
2017-08-12 00:47:18 +0530

    Create config.php

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+	$con=mysqli_connect("localhost","root","","base");
+	
+	if(!$con)
+	{
+		die("unable to connect");    // or exit();
+	}
+	
+	else
+	{
+		//echo "successfully connected";
+	}
+?>
